ee707564fca42c2936d5aeaeb5b9dd53f6d8263d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-13 20:48:50 +0000

    Deploy kfarnung/puzztool to github.com/kfarnung/puzztool.git:gh-pages

---
+++ precache-manifest.c2cdc4923d438e09f895a38651d24595.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06442c663a7593dbbf89204eeb03bba5",
+    "url": "/puzztool/index.html"
+  },
+  {
+    "revision": "bb273a1faec956a2c9c1",
+    "url": "/puzztool/static/css/10.6e57a530.chunk.css"
+  },
+  {
+    "revision": "095ac597e6bea5fb2d31",
+    "url": "/puzztool/static/css/11.eea080ed.chunk.css"
+  },
+  {
+    "revision": "03b4cd9ed54c483cdc58",
+    "url": "/puzztool/static/css/12.9e7a7a67.chunk.css"
+  },
+  {
+    "revision": "3eda59118498d2b4b42a",
+    "url": "/puzztool/static/css/13.4a38f055.chunk.css"
+  },
+  {
+    "revision": "eca12c562228f079699c",
+    "url": "/puzztool/static/css/14.774185b9.chunk.css"
+  },
+  {
+    "revision": "845f3bb2c77639cfea72",
+    "url": "/puzztool/static/css/15.e9c452e3.chunk.css"
+  },
+  {
+    "revision": "b878aac362042891f74e",
+    "url": "/puzztool/static/css/16.5cf3d16b.chunk.css"
+  },
+  {
+    "revision": "184efe95513b8aab7708",
+    "url": "/puzztool/static/css/17.c8b40d14.chunk.css"
+  },
+  {
+    "revision": "583ecc4cb8ac36c250b9",
+    "url": "/puzztool/static/css/18.351ac8b4.chunk.css"
+  },
+  {
+    "revision": "1b1b216e3838f5cb6fbc",
+    "url": "/puzztool/static/css/19.6eeb1f2e.chunk.css"
+  },
+  {
+    "revision": "7823eb9e0d25de875661",
+    "url": "/puzztool/static/css/4.03b1dffa.chunk.css"
+  },
+  {
+    "revision": "96fac664807a109c395b",
+    "url": "/puzztool/static/css/5.936487ff.chunk.css"
+  },
+  {
+    "revision": "ad216db7c613d28ed8af",
+    "url": "/puzztool/static/css/6.fbd53a73.chunk.css"
+  },
+  {
+    "revision": "0d49e975d87b95906349",
+    "url": "/puzztool/static/css/7.66ea3997.chunk.css"
+  },
+  {
+    "revision": "39449bee34497e1036ae",
+    "url": "/puzztool/static/css/8.b9d08866.chunk.css"
+  },
+  {
+    "revision": "0bce82278551ff706aaa",
+    "url": "/puzztool/static/css/9.bd195aa9.chunk.css"
+  },
+  {
+    "revision": "b0d41c9ceede8cf73abd",
+    "url": "/puzztool/static/css/main.ac23667b.chunk.css"
+  },
+  {
+    "revision": "da709ca74eaca8b393aa",
+    "url": "/puzztool/static/js/0.5d65791a.chunk.js"
+  },
+  {
+    "revision": "709d6642cad6d1ff52eb",
+    "url": "/puzztool/static/js/1.3dc64668.chunk.js"
+  },
+  {
+    "revision": "bb273a1faec956a2c9c1",
+    "url": "/puzztool/static/js/10.7ecbd7cf.chunk.js"
+  },
+  {
+    "revision": "095ac597e6bea5fb2d31",
+    "url": "/puzztool/static/js/11.51612973.chunk.js"
+  },
+  {
+    "revision": "03b4cd9ed54c483cdc58",
+    "url": "/puzztool/static/js/12.82b8a17c.chunk.js"
+  },
+  {
+    "revision": "3eda59118498d2b4b42a",
+    "url": "/puzztool/static/js/13.6b35a184.chunk.js"
+  },
+  {
+    "revision": "eca12c562228f079699c",
+    "url": "/puzztool/static/js/14.ed94987d.chunk.js"
+  },
+  {
+    "revision": "845f3bb2c77639cfea72",
+    "url": "/puzztool/static/js/15.d86665e9.chunk.js"
+  },
+  {
+    "revision": "b878aac362042891f74e",
+    "url": "/puzztool/static/js/16.d11d48c7.chunk.js"
+  },
+  {
+    "revision": "184efe95513b8aab7708",
+    "url": "/puzztool/static/js/17.2bd7435b.chunk.js"
+  },
+  {
+    "revision": "583ecc4cb8ac36c250b9",
+    "url": "/puzztool/static/js/18.3dc68eb6.chunk.js"
+  },
+  {
+    "revision": "1b1b216e3838f5cb6fbc",
+    "url": "/puzztool/static/js/19.40c576c6.chunk.js"
+  },
+  {
+    "revision": "7823eb9e0d25de875661",
+    "url": "/puzztool/static/js/4.cfeb992b.chunk.js"
+  },
+  {
+    "revision": "96fac664807a109c395b",
+    "url": "/puzztool/static/js/5.ce9a3281.chunk.js"
+  },
+  {
+    "revision": "ad216db7c613d28ed8af",
+    "url": "/puzztool/static/js/6.0f020b3b.chunk.js"
+  },
+  {
+    "revision": "0d49e975d87b95906349",
+    "url": "/puzztool/static/js/7.6ab59a12.chunk.js"
+  },
+  {
+    "revision": "39449bee34497e1036ae",
+    "url": "/puzztool/static/js/8.04b5ed59.chunk.js"
+  },
+  {
+    "revision": "0bce82278551ff706aaa",
+    "url": "/puzztool/static/js/9.008775e0.chunk.js"
+  },
+  {
+    "revision": "b0d41c9ceede8cf73abd",
+    "url": "/puzztool/static/js/main.21a52273.chunk.js"
+  },
+  {
+    "revision": "29688c9665ae221b7062",
+    "url": "/puzztool/static/js/runtime~main.4c5141cd.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/puzztool/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/puzztool/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/puzztool/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/puzztool/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/puzztool/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "d6ee0a091b227e756c6eee6661d74252",
+    "url": "/puzztool/static/media/pigpen_key.d6ee0a09.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/puzztool/precache-manifest.f30408294e1131f322b2c6cf5199a51b.js"
+  "/puzztool/precache-manifest.c2cdc4923d438e09f895a38651d24595.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/puzztool/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puzztool/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
