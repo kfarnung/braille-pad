d95600372ffc1ee10d033dc99bf82c24ac5dfa1a
Kyle Farnung <kfarnung@outlook.com>
2019-08-31 09:54:06 -0700

    Remove usage of react-loadable
    The `react-loadable` package is generating warnings when building due to
    usage of deprecated APIs. There are other alternative packages, but most
    of the functionality is built directly into React.
    
    Implement using `React.lazy`, `Suspense`, and `ErrorBoundary` which are
    built-in concepts. To avoid a boilerplate `ErrorBoundary`
    implementation, the `react-error-boundary` package is used. In the event
    that becomes an issue, the implementation is pretty simple.

--- src/App.tsx
+++ src/App.tsx
@@ -1,131 +1,128 @@
-import { Route, Switch } from 'react-router-dom';
-import Loadable from 'react-loadable';
-import PuzzToolDocumentTitle from './Common/PuzzToolDocumentTitle';
-import AppNav from './Features/AppNav';
-import NotFound from './Features/Error/NotFound';
-import Loading from './Features/Loading';
-import './App.scss';
+import React, { Suspense } from "react";
+import ErrorBoundary from "react-error-boundary";
+import {
+  Route,
+  RouteComponentProps,
+  Switch,
+  withRouter,
+} from "react-router-dom";
+import PuzzToolDocumentTitle from "./Common/PuzzToolDocumentTitle";
+import AppNav from "./Features/AppNav";
+import NotFound from "./Features/Error/NotFound";
+import Loading from "./Features/Loading";
+import "./App.scss";
 
-const defaultTimeout = 10000;
+const loadingDelay = 200;
+const loadingTimeout = 10000;
 
-const WordSearch = Loadable({
-  loader: () => import('./Features/WordSearch'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
+const Autokey = React.lazy(() => import("./Features/Cipher/Autokey"));
+const Caesar = React.lazy(() => import("./Features/Cipher/Caesar"));
+const Vigenere = React.lazy(() => import("./Features/Cipher/Vigenere"));
+const AutoConvert = React.lazy(() => import("./Features/Encoding/AutoConvert"));
+const Braille = React.lazy(() => import("./Features/Encoding/Braille"));
+const Character = React.lazy(() => import("./Features/Encoding/Character"));
+const Morse = React.lazy(() => import("./Features/Encoding/Morse"));
+const Nato = React.lazy(() => import("./Features/Encoding/Nato"));
+const NavalFlag = React.lazy(() => import("./Features/Encoding/NavalFlag"));
+const Semaphore = React.lazy(() => import("./Features/Encoding/Semaphore"));
+const Home = React.lazy(() => import("./Features/Home"));
+const Pigpen = React.lazy(() => import("./Features/Pigpen"));
+const Resistor = React.lazy(() => import("./Features/Resistor"));
+const Settings = React.lazy(() => import("./Features/Settings"));
+const WordSearch = React.lazy(() => import("./Features/WordSearch"));
 
-const AutoConvert = Loadable({
-  loader: () => import('./Features/Encoding/AutoConvert'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
+function ErrorFallback() {
+  return (
+    <div className="App-loadError">
+      <div className="App-loadError-icon">!</div>
+      <div>Failed to load content</div>
+    </div>
+  );
+}
 
-const Autokey = Loadable({
-  loader: () => import('./Features/Cipher/Autokey'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const Braille = Loadable({
-  loader: () => import('./Features/Encoding/Braille'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const Caesar = Loadable({
-  loader: () => import('./Features/Cipher/Caesar'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const Character = Loadable({
-  loader: () => import('./Features/Encoding/Character'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const Home = Loadable({
-  loader: () => import('./Features/Home'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const Morse = Loadable({
-  loader: () => import('./Features/Encoding/Morse'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const Nato = Loadable({
-  loader: () => import('./Features/Encoding/Nato'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const NavalFlag = Loadable({
-  loader: () => import('./Features/Encoding/NavalFlag'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const Pigpen = Loadable({
-  loader: () => import('./Features/Pigpen'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const Resistor = Loadable({
-  loader: () => import('./Features/Resistor'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const Semaphore = Loadable({
-  loader: () => import('./Features/Encoding/Semaphore'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const Settings = Loadable({
-  loader: () => import('./Features/Settings'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-const Vigenere = Loadable({
-  loader: () => import('./Features/Cipher/Vigenere'),
-  loading: Loading,
-  timeout: defaultTimeout,
-});
-
-function App() {
+function App(props: RouteComponentProps) {
   return (
     <PuzzToolDocumentTitle>
       <div className="App">
         <AppNav />
         <div className="App-content">
-          <Switch>
-            <Route exact={true} path="/" component={Home} />
-            <Route exact={true} path="/cipher/autokey" component={Autokey} />
-            <Route exact={true} path="/cipher/caesar" component={Caesar} />
-            <Route exact={true} path="/cipher/vigenere" component={Vigenere} />
-            <Route exact={true} path="/encoding/autoconvert" component={AutoConvert} />
-            <Route exact={true} path="/encoding/braille" component={Braille} />
-            <Route exact={true} path="/encoding/morse" component={Morse} />
-            <Route exact={true} path="/encoding/pigpen" component={Pigpen} />
-            <Route exact={true} path="/encoding/semaphore" component={Semaphore} />
-            <Route exact={true} path="/help/settings" component={Settings} />
-            <Route exact={true} path="/reference/characterencodings" component={Character} />
-            <Route exact={true} path="/reference/nato" component={Nato} />
-            <Route exact={true} path="/reference/navalflags" component={NavalFlag} />
-            <Route exact={true} path="/reference/resistors" component={Resistor} />
-            <Route exact={true} path="/solvers/wordsearch" component={WordSearch} />
-            <Route component={NotFound} />
-          </Switch>
+          <ErrorBoundary
+            FallbackComponent={ErrorFallback}
+            key={props.history.location.key}
+          >
+            <Suspense
+              fallback={
+                <Loading delay={loadingDelay} timeout={loadingTimeout} />
+              }
+            >
+              <Switch>
+                <Route exact={true} path="/" component={Home} />
+                <Route
+                  exact={true}
+                  path="/cipher/autokey"
+                  component={Autokey}
+                />
+                <Route exact={true} path="/cipher/caesar" component={Caesar} />
+                <Route
+                  exact={true}
+                  path="/cipher/vigenere"
+                  component={Vigenere}
+                />
+                <Route
+                  exact={true}
+                  path="/encoding/autoconvert"
+                  component={AutoConvert}
+                />
+                <Route
+                  exact={true}
+                  path="/encoding/braille"
+                  component={Braille}
+                />
+                <Route exact={true} path="/encoding/morse" component={Morse} />
+                <Route
+                  exact={true}
+                  path="/encoding/pigpen"
+                  component={Pigpen}
+                />
+                <Route
+                  exact={true}
+                  path="/encoding/semaphore"
+                  component={Semaphore}
+                />
+                <Route
+                  exact={true}
+                  path="/help/settings"
+                  component={Settings}
+                />
+                <Route
+                  exact={true}
+                  path="/reference/characterencodings"
+                  component={Character}
+                />
+                <Route exact={true} path="/reference/nato" component={Nato} />
+                <Route
+                  exact={true}
+                  path="/reference/navalflags"
+                  component={NavalFlag}
+                />
+                <Route
+                  exact={true}
+                  path="/reference/resistors"
+                  component={Resistor}
+                />
+                <Route
+                  exact={true}
+                  path="/solvers/wordsearch"
+                  component={WordSearch}
+                />
+                <Route component={NotFound} />
+              </Switch>
+            </Suspense>
+          </ErrorBoundary>
         </div>
       </div>
     </PuzzToolDocumentTitle>
   );
 }
 
-export default App;
+export default withRouter(App);
--- src/Features/Loading/index.tsx
+++ src/Features/Loading/index.tsx
@@ -1,39 +1,63 @@
-import { LoadingComponentProps } from 'react-loadable';
-import './index.scss';
+import React, { useState, useEffect } from "react";
+import "./index.scss";
 
-function Loading(props: LoadingComponentProps) {
+interface Props {
+  delay?: number;
+  timeout?: number;
+}
+
+function Loading(props: Props) {
+  const [pastDelay, setPastDelay] = useState(false);
+  const [timedOut, setTimedOut] = useState(false);
+
+  useEffect(() => {
+    const delay = props.delay;
+    const timeout = props.timeout;
+    let delayTimer: number;
+    let timeoutTimer: number;
+
+    if (delay && delay > 0) {
+      delayTimer = window.setTimeout(() => setPastDelay(true), delay);
+    } else {
+      setPastDelay(true);
+    }
+
+    if (timeout && timeout > 0) {
+      timeoutTimer = window.setTimeout(() => setTimedOut(true), timeout);
+    }
+
+    return () => {
+      if (delayTimer) {
+        clearTimeout(delayTimer);
+      }
+
+      if (timeoutTimer) {
+        clearTimeout(timeoutTimer);
+      }
+    };
+  }, [props.delay, props.timeout]);
+
   function getContent() {
-    if (props.error) {
-      return (
-        <div className="Loading-content">
-          <div className="Loading-error" />
-          <div>Failed to load content</div>
-        </div>
-      );
-    } else if (props.timedOut) {
+    if (timedOut) {
       return (
         <div className="Loading-content">
           <div className="Loading-error" />
           <div>Timed out while loading content</div>
         </div>
       );
-    } else if (props.pastDelay) {
+    } else if (pastDelay) {
       return (
         <div className="Loading-content">
           <div className="Loading-spinner" />
           <div>Loading</div>
         </div>
       );
-    } else {
-      return null;
     }
+
+    return null;
   }
 
-  return (
-    <div className="Loading">
-      {getContent()}
-    </div>
-  );
+  return <div className="Loading">{getContent()}</div>;
 }
 
 export default Loading;
