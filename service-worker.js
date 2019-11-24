ca89bf8bdd433fb4a2a5e3e671dfe59e330cabfc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-24 00:57:21 +0000

    Deploy kfarnung/puzztool to github.com/kfarnung/puzztool.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cbfe6f8be300f48831b17298ebed4dd8.js"
+  "/puzztool/precache-manifest.f2458e45f82600a1064f2f2d1bfdd17d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puzztool/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
